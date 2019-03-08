178a8f544526289fc923c197b7ef9b9e584c368e
Joseph Herbert Tandingan <jh.tandingan@coi.local>
2019-03-08 15:09:10 +0800

    Change text functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text = 'Default starting text';
 
+  onChangeText() {
+    this.text = 'Changed'
+  }
 }
